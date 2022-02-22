00f150f65e0320638ba580bf05703f65cf8a4c54
Zhuliang Geng <zhuliang.geng@uwaterloo.ca>
2022-02-21 20:49:25 -0500

    update sql

--- create db.sql
+++ create db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `job`.`jobs` (
-  `glassdoor_id` INT NULL,
+  `glassdoor_id` TEXT NULL,
   `title` TEXT NOT NULL,
   `publish_time` INT NULL,
-  `description` TEXT NULL,
+  `description` LONGTEXT NULL,
   `company` TEXT NOT NULL,
   `locations` TEXT NULL,
-  `apply_url` TEXT NULL,
-  `from_url` VARCHAR(600) NOT NULL,
+  `apply_url` LONGTEXT NULL,
+  `from_url` LONGTEXT NOT NULL,
   `basic_qualifications` TEXT NULL,
   `team` TEXT NULL,
   `city` TEXT NULL,
@@ -18,10 +18,10 @@ CREATE TABLE `job`.`jobs` (
   `new_grad` TINYINT(1) NULL,
   `has_remote` TINYINT(1) NULL,
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `subcompany` TEXT NOT NULL,
+  `subcompany` TEXT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `from_url_UNIQUE` (`from_url` ASC) VISIBLE,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
+
 
