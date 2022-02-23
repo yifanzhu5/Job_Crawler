79015d42b83480f8be90b76d66998033ed3053bc
Anyi Shi <a26shi@uwaterloo.ca>
2022-02-22 21:31:50 -0500

    update create db

--- jobSearch/jobSearch/spiders/shopify_jobs.py
+++ jobSearch/jobSearch/spiders/shopify_jobs.py
@@ -7,24 +7,10 @@ import datetime
 import time
 import re
 from parsel import Selector
+from bs4 import BeautifulSoup
 
 
 class ShopifyJobsSpider(scrapy.Spider):
-    '''
-    f = open('Shopify.csv', mode='a', encoding='utf-8', newline='')
-    csv_writer = csv.DictWriter(f, fieldnames=['title',
-                                               'company',
-                                               'locations',
-                                               'team',
-                                               'new_grad',
-                                               'description',
-                                               'apply_url',
-                                               'from_url',
-                                               ])
-    csv_writer.writeheader()
-    t1 = datetime.datetime.now()
-    '''
-
     name = 'shopify_jobs'
     url = 'https://www.shopify.com/careers/search?teams%5B%5D=data&teams%5B%5D=engineering&teams%5B%5D=interns&locations%5B%5D=Americas&locations%5B%5D=Canada&keywords=&sort=team_asc'
     timestamp = int(time.time())
@@ -55,20 +41,23 @@ class ShopifyJobsSpider(scrapy.Spider):
             'table.job-info__table:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > a:nth-child(1)::text').get()
         title_data = selector.css('head > title:nth-child(7)::text').get().split(' | ')[0]
         if title_data.find('United States') == -1:  # not found
-            # if title_data.find('Remote')!=-1:
-            #     Remote_data=True
-            # else:
-            #     Remote_data = False
             team_data = selector.css(
                 'table.job-info__table:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > a:nth-child(1)::text').get()
 
-            JD_data = selector.css(
-                '.job-posting__grid-content').get()  # save all job description (including About the role, Qualifications, How we hire) and their content as a css selector
-            # About_the_role_data = selector.css(
-            #     '.job-posting__grid-content > div:nth-child(1)')  # save "About the role" and content as a css selector
-            # Qualifications_data=selector.css('.job-posting__grid-content > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)')
+
 
             Apply_now_data = selector.css('.button-light--indigo::attr(href)').get()
+
+            JD_data = selector.css(
+                '.job-posting__grid-content').get()  # save all job description (including About the role, Qualifications, How we hire) and their content as a css selector
+
+            soup = BeautifulSoup(JD_data, 'html.parser')
+            h2 = soup.find_all('h2')
+            for h in h2:
+                h.name = "h4"
+            a = soup.find_all('a')
+            a[len(a)-1].attrs['href']="https://www.shopify.com"+a[len(a)-1].attrs['href']
+            JD_data = soup.prettify().replace("\n", "")
 
             if team_data == "Internships":
                 new_graduate = True
@@ -93,15 +82,3 @@ class ShopifyJobsSpider(scrapy.Spider):
             item["has_remote"] = remote_data
 
             yield item
-            # Dict = {
-            #     "title": title_data,
-            #     "company": "Shopify",
-            #     "locations": location_data,
-            #     "team": team_data,
-            #     "apply_url": Apply_now_data,
-            #     "new_grad": new_graduate,
-            #     "description": JD_data,
-            #     "from_url": reqUrl,
-            #     # "remote":Remote_data
-            # }
-            # csv_writer.writerow(Dict)
